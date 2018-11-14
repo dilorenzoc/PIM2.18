8f07a8294787234ba6017f82070a273222b787c4
Camila Di Lorenzo <43864961+dilorenzoc@users.noreply.github.com>
2018-11-14 20:23:00 -0200

    Add files via upload

---
+++ data sys.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <time.h>
+
+int main(void) {
+    time_t mytime;
+    mytime = time(NULL);
+    struct tm tm = *localtime(&mytime);
+    printf("Data: %d/%d/%d/\n", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900);
+}
